063946861eaea1e624a8ca14bbbabf195232688f
unknown <eimanhamdy99@gmail.com>
2024-09-19 02:13:21 +0200

    add hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
 import latestphone from "../assets/latestPhone.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faApple } from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
   return (
     <>
       <section className="hero">
+        {/* <h3 className="title line1">Core Product,</h3>
+        <h3 className="title line2">Prime Performance</h3> */}
+        <div className="productName">
+          <p>iPhone 14 Pro</p>
+
+          <FontAwesomeIcon icon={faApple} />
+
+          <h4>Now Available</h4>
+        </div>
         <div className="latest-release">
           <img src={latestphone} />
         </div>
