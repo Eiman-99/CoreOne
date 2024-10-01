657babfea8825344543dad31014ab278dac48531
unknown <eimanhamdy99@gmail.com>
2024-10-01 21:48:37 +0200

    update hero section

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,7 @@
 import Carousel from "react-bootstrap/Carousel";
 import latestPhone from "../assets/latestPhone.png";
+import latestAirpods from "../assets/latestAirpods.png";
+import latestWatch from "../assets/latestWatch.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faApple } from "@fortawesome/free-brands-svg-icons";
 
@@ -7,34 +9,34 @@ function UncontrolledExample() {
   return (
     <Carousel controls={false} indicators={false}>
       <Carousel.Item>
-        <img src={latestPhone} />
-        <Carousel.Caption>
-          <div className="productName">
-            <p>iPhone 14 Pro</p>
-            <FontAwesomeIcon icon={faApple} />
-            <h4>Now Available</h4>
-          </div>
-        </Carousel.Caption>
+        <div className="img-container">
+          <img src={latestPhone} />
+        </div>
+        <div className="productName responsive">
+          <p>iPhone 15 Pro</p>
+          <FontAwesomeIcon icon={faApple} />
+          <h4>Now Available</h4>
+        </div>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={latestPhone} />
-        <Carousel.Caption>
-          <div className="productName">
-            <p>iPhone 14 Pro</p>
-            <FontAwesomeIcon icon={faApple} />
-            <h4>Now Available</h4>
-          </div>
-        </Carousel.Caption>
+        <div className="img-container">
+          <img src={latestAirpods} />
+        </div>
+        <div className="productName responsive">
+          <p>Airpods Pro</p>
+          <FontAwesomeIcon icon={faApple} />
+          <h4>Now Available</h4>
+        </div>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={latestPhone} />
-        <Carousel.Caption>
-          <div className="productName">
-            <p>iPhone 14 Pro</p>
-            <FontAwesomeIcon icon={faApple} />
-            <h4>Now Available</h4>
-          </div>
-        </Carousel.Caption>
+        <div className="img-container">
+          <img src={latestWatch} />
+        </div>
+        <div className="productName responsive">
+          <p>Apple Watch S9</p>
+          <FontAwesomeIcon icon={faApple} />
+          <h4>Now Available</h4>
+        </div>
       </Carousel.Item>
     </Carousel>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,18 +10,6 @@ export default function Home() {
   return (
     <>
       <section className="hero">
-        {/* <h3 className="title line1">Core Product,</h3>
-        <h3 className="title line2">Prime Performance</h3> */}
-        {/* <div className="productName">
-          <p>iPhone 14 Pro</p>
-
-          <FontAwesomeIcon icon={faApple} />
-
-          <h4>Now Available</h4>
-        </div> */}
-        {/* <div className="latest-release">
-          <img src={latestphone} alt="hero-image" />
-        </div> */}
         <Carousel />
       </section>
       <section id="categories">
