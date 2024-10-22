32ad5b54323ed5df8b4c1b1a02cab3ac3e58014a
unknown <eimanhamdy99@gmail.com>
2024-10-22 10:53:50 +0200

    adjust checkout UI

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -24,6 +24,7 @@ export default function CategoryPage() {
   if (!currentCategory) {
     return <Loading />;
   }
+
   return (
     <div className="categoryPage section-py">
       <div className="category-container">
